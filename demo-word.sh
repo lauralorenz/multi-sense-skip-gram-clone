ce3b584c1b45e29f9a2b503659bb57446ab47934
Jeevan Shankar <jshankar@blake.cs.umass.edu>
2014-09-15 21:34:32 -0400

    update demo-word.sh

--- demo-word.sh
+++ demo-word.sh
@@ -19,16 +19,9 @@ sam="0.001"
 stopwords="1"
 mv="5000"
 
-input=""
-case ${data} in 
-    rcv-wiki) input="/iesl/canvas/jshankar/rcv-wiki_clean.txt";;
-    rcv-wiki-phrase) input="/iesl/canvas/jshankar/socher-data/rcv-wiki-phrase-3";;
-    socher-wiki-match) input="/iesl/canvas/jshankar/socher-data/socher-wiki-match.txt";;
-    text8) input=text8_linebreak ;; 
-esac
-bootvocab="/iesl/canvas/jshankar/vectors/vocab/${data}.vocab.gz"
-multivocab="/iesl/canvas/jshankar/vectors/vocab/socher-only-multi.vocab"
-output="vec_${data}_${dim}d_${num_sense}s_${num_neg}num_neg_${v}v_${sam}sam_${stopwords}stopwords_${mv}mv.txt.gz"
+#bootvocab="/iesl/canvas/jshankar/vectors/vocab/${data}.vocab.gz"
+#multivocab="/iesl/canvas/jshankar/vectors/vocab/socher-only-multi.vocab"
+#output="vec_${data}_${dim}d_${num_sense}s_${num_neg}num_neg_${v}v_${sam}sam_${stopwords}stopwords_${mv}mv.txt.gz"
 
 
-${wordvec_app} --use-k-means=1 --learn-top-v=${mv} --load-multi-vocab=${multivocab} --cbow=2 --rate=0.025 --train ${input} --output ${output} --size=${dim} --window=5  --min-count=11 --threads=23 --negative=${num_neg} --sample=${sam} --sense=${num_sense} --load-vocab=${bootvocab} --binary=1 --ignore-stopwords=${stopwords} --encoding=ISO-8859-15 --max-vocab-size=$v
+${wordvec_app} --use-k-means=1 --learn-top-v=2000 --cbow=2 --train text8 --output vectors_MSSG.gz --size=300 --window=5  --min-count=11 --threads=23 --negative=1 --sample=0.001 --sense=2 --binary=1 --ignore-stopwords=1 --encoding=ISO-8859-15 --save-vocab=text8.vocab.gz
